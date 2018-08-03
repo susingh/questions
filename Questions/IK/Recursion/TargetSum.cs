f942be7fe462c9630749a75aa1df8d4e8fd2ca5b
Sumit Singh <sumit.singh@outlook.com>
2018-08-02 21:12:11 -0700

    Adding recursion test retake problems.

---
+++ Questions/IK/Recursion/TargetSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions.IK.Recursion
+{
+    class TargetSum
+    {
+        public static bool check_if_sum_possible(long[] arr, long k)
+        {
+            return check_if_sum_possible(arr, 0, k, false);
+        }
+
+        private static bool check_if_sum_possible(long[] input, int i, long sum, bool hasConsideredElements)
+        {
+            if (sum == 0 && hasConsideredElements)
+            {
+                return true;
+            }
+
+            if (i == input.Length)
+            {
+                return false;
+            }
+
+            return check_if_sum_possible(input, i + 1, sum, false) || check_if_sum_possible(input, i + 1, sum - input[i], true);
+        }
+    }
+}
---
+++ Questions/IK/Recursion/WellFormedBrackets.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions.IK.Recursion
+{
+    class WellFormedBrackets
+    {
+        public static string[] find_all_well_formed_brackets(int n)
+        {
+            char[] arr = new char[n * 2];
+            List<string> result = new List<string>();
+            find_all_well_formed_brackets(arr, 0, result);
+            return result.ToArray();
+        }
+
+        private static void find_all_well_formed_brackets(char[] arr, int i, List<string> result)
+        {
+            if (i == arr.Length)
+            {
+                if (IsWellFormed(arr, i - 1))
+                {
+                    string val = new string(arr);
+                    result.Add(val);
+                }
+                return;
+            }
+           
+            char[] values = new char[] { '(', ')' };
+            foreach (var bracket in values)
+            {
+                arr[i] = bracket;
+                find_all_well_formed_brackets(arr, i + 1, result);
+            }
+        }
+
+        private static bool IsWellFormed(char[] arr, int j)
+        {
+            int opened = 0;
+            for (int i = 0; i <= j; i++)
+            {
+                if (arr[i] == ')')
+                {
+                    opened--;
+
+                    if (opened < 0)
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    opened++;
+                }
+            }
+
+            return opened == 0;
+        }
+    }
+}
